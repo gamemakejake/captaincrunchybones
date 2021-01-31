fced8cee16e54a74a43197d4bf41c8d2b1001465
Rhyden <53986293+RhydenRohilla@users.noreply.github.com>
2021-01-30 18:48:19 -0800

    Cannon Go Boom and Dog go Bark

---
+++ CaptCrunchyBones/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public enum GAMESTATE { Cannon, Dog, End }
+    public GAMESTATE currentGameState;
+
+    public GameObject player;
+    public void Start()
+    {
+        //Cursor actually performs like in an FPS
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void Update()
+    {
+    }
+
+    public IEnumerator SwitchToPlayerState()
+    {
+        yield return new WaitForSeconds(1);
+        player.SetActive(true);
+        player.GetComponent<PlayerController>().numBarks = 0;
+        currentGameState = GAMESTATE.Dog;
+    }
+}
